e08177dfa4da07cfffce7c2edb418cbb0852ed47
Emilia <emiliastefanowska@outlook.com>
2022-03-26 07:37:10 +0100

    Modifying Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main (String[] args) {
 
+        System.out.println(NumericExercises.getLargestPrime(21));
+        System.out.println(NumericExercises.getLargestPrime(217));
+        System.out.println(NumericExercises.getLargestPrime(0));
+        System.out.println(NumericExercises.getLargestPrime(45));
+        System.out.println(NumericExercises.getLargestPrime(-1));
     }
-
 }
