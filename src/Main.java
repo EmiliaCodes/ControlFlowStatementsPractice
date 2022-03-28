2f0578feee951697db8997bf613b1d6899557227
Emilia <emiliastefanowska@outlook.com>
2022-03-28 16:40:54 +0200

    Checking out new exercise.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main (String[] args) {
 
         /* Trying out printSquareStar method */
-        OtherExercises.printSquareStar(5);
-        OtherExercises.printSquareStar(10);
+//        OtherExercises.printSquareStar(5);
+//        OtherExercises.printSquareStar(10);
+//        System.out.println(System.getProperty("java.io"));
+
+        /* Trying out minAndMaxFromInput method*/
+        ReadingUserInput.minAndMaxFromInput();
     }
 }
